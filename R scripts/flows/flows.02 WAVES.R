f65acf382bbeef8824a279bf9d0b8e26c67e50b9
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-01-17 11:28:58 +0000

    feat: Update wave data to ERA5

--- R scripts/flows/flows.02 WAVES.R
+++ R scripts/flows/flows.02 WAVES.R
@@ -13,25 +13,22 @@ domains <- readRDS("./Objects/Domains.rds") %>%                             # Im
   filter(Shore == "Inshore") %>% 
   st_transform(crs = 4326)
 
-waves <- brick("../Sediment/Objects/waves.nc", varname = "swh")             # Import significant wave height time series
+waves <- brick("../Shared data/Waves/waves.nc", varname = "swh")            # Import significant wave height time series
 
 #### Calculate mean disturbance per habitat area, weighting by cell coverage ####
 
 values <- exact_extract(waves, domains, fun = 'mean') %>%                   # Extract inshore waves    
   pivot_longer(everything(), names_to = "Date", values_to = "Waves") %>%    # Move time steps into a single column
   mutate(Date = str_remove(Date, "mean.X")) %>%                             # Fix date string
-  mutate(Date = as.POSIXct(Date, format = "%Y.%m.%d.%H.%M.%S"),             # Format as date
-         Month = lubridate::month(Date)) %>%                                # Extract month
-  group_by(Month) %>% 
-  summarise(Waves = mean(Waves))                                            # Mean by month
+  mutate(Date = as.POSIXct(Date, format = "%Y.%m.%d"))                      # Format as date
 
 saveRDS(values, "./Objects/Significant wave height.rds")
 
 ggplot(values) +
-  geom_line(aes(x = Month, y = Waves)) +
+  geom_line(aes(x = Date, y = Waves)) +
   theme_minimal() +
-  scale_x_continuous(breaks = c(0, 3, 6, 9, 12)) +
-  labs(y = "Mean significant wave height (m)", x = "Month", caption = "Data from ECMWF Cera-20C") +
+#  scale_x_continuous(breaks = c(0, 3, 6, 9, 12)) +
+  labs(y = "Mean significant wave height (m)", x = "Month", caption = "Data from ERA5 reanalysis") +
 NULL
 
 ggsave("./Figures/flows/Significant wave height.png", width = 16, height = 8, units = "cm")
