d4baa04bfd3854e92fc7ba8322e0cb1e9386dad4
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-03-11 15:39:05 +0000

    feat: create dummy implementation based on Celtic Sea.

---
+++ R scripts/se2e/strathE2E.01 INITIALISE MODEL.R
@@ -0,0 +1,7 @@
+
+## Initialise model
+
+implementation <- "Norwegian_Shelf"
+R.utils::copyDirectory("../Celtic Sea/Data/Celtic_Sea_ERSEM/", stringr::str_glue("./StrathE2E/{implementation}/")) # Copy example model
+
+#!!# Rename folder to appropriate reference period
